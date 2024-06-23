d2329fddd80359d6496e26e8daf719a2f3b0837e
stepanchous <s.kolovorotnyy@gmail.com>
2024-06-23 10:48:06 +0300

    update repo check

---
+++ lesson6/merge_arrays.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  int a = 5;
+  int b = 5;
+
+  int c = ++a;
+  int d = b++;
+
+  printf("c: %d, d: %d\n", c, d);
+  printf("a: %d, b: %d\n", a, b);
+
+  for (int i = 0; i < 19; ++i) {
+
+  }
+}
